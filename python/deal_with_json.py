6b695b9262b3a00d2ef748d90b82315fe1e5cdfe
juscodit <juscodit@gmail.com>
2015-10-10 09:15:34 +0800

    update deal_with_json.py

--- python/deal_with_json.py
+++ python/deal_with_json.py
@@ -7,6 +7,14 @@ def main():
   print jsn["name"]
   print jsn["albums"]
 
+  # dump json to string
+  # @indent and @separators are for pretty print
+  print json.dumps(jsn, sort_keys=True, indent=4, separators=(',', ': '))
+
+  # use json.loads() if load directly from string
+  jsn = json.loads('{ "name": "xxx", "age": "28" }')
+  print jsn["name"]
+
 if __name__ == '__main__':
   main()
 
