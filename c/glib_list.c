50340952f7e01a7488b7d85cc10c58ed36a14d0e
juscodit <juscodit@gmail.com>
2015-10-14 19:02:26 +0800

    update glib_list.c commend

--- c/glib_list.c
+++ c/glib_list.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 {
     GList *lst = NULL;
 
+    /* store integers */
     lst = g_list_append(lst, (gpointer)3);
     lst = g_list_append(lst, (gpointer)5);
     lst = g_list_append(lst, (gpointer)6);
@@ -18,6 +19,8 @@ int main(int argc, char *argv[])
     g_list_foreach(lst, print, NULL);
     g_list_free(lst);
 
+    /* store dynamically allocated data, such list should be freed
+     * with g_list_free_full() */
     lst = NULL;
     lst = g_list_append(lst, (gpointer)g_strdup("hello"));
     lst = g_list_append(lst, (gpointer)g_strdup("world"));
