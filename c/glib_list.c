224946bf1cd6bc28df149d6e0dab53cb98fddcc1
juscodit <juscodit@gmail.com>
2015-10-14 18:53:37 +0800

    update

--- c/glib_list.c
+++ c/glib_list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
 
 void print(gpointer item, gpointer user_data)
@@ -12,8 +13,15 @@ int main(int argc, char *argv[])
 
     lst = g_list_append(lst, (gpointer)3);
     lst = g_list_append(lst, (gpointer)5);
+    lst = g_list_append(lst, (gpointer)6);
 
     g_list_foreach(lst, print, NULL);
+    g_list_free(lst);
+
+    lst = NULL;
+    lst = g_list_append(lst, (gpointer)g_strdup("hello"));
+    lst = g_list_append(lst, (gpointer)g_strdup("world"));
+    g_list_free_full(lst, free);
 
     return 0;
 }
