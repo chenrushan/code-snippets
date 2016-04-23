fb89b557764ff8ed6ff3235dcea62d09ce8a8fa0
juscodit <juscodit@gmail.com>
2016-04-23 15:05:46 +0800

    [boost_filesystem]: add more demo

--- cpp/boost_filesystem.cpp
+++ cpp/boost_filesystem.cpp
@@ -1,26 +1,33 @@
 // compile with -lboost_filesystem -lboost_system
 #include <boost/filesystem.hpp>
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     // check if a file exists
     if (!boost::filesystem::exists("boost_filesystem.cpp")) {
-        std::cout << "file not exists" << std::endl;
+        cout << "file not exists" << endl;
     } else {
-        std::cout << "file exists" << std::endl;
+        cout << "file exists" << endl;
     }
 
     // remove a file
     boost::filesystem::remove("xx");
 
     // get file size
-    std::cout << boost::filesystem::file_size("boost_filesystem.cpp") << std::endl;
+    cout << boost::filesystem::file_size("boost_filesystem.cpp") << endl;
 
     // get filename
     boost::filesystem::path p("/home/crs/kgengine.tar.gz");
-    std::cout << "path" << p << std::endl;
-    std::cout << "filename " << p.filename().string() << std::endl;
-    std::cout << "filename without extension " << p.stem().string() << std::endl;
+    cout << "path" << p.string() << endl;
+    cout << "filename " << p.filename().string() << endl;
+    cout << "filename without extension " << p.stem().string() << endl;
+    cout << p.parent_path().string() << endl;
+
+    // remove extension
+    boost::filesystem::path p2 = p.replace_extension("");
+    cout << p2.string() << endl;
     
     return 0;
 }
