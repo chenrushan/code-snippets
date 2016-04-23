9d84eabab6875ba4285f8e651f0805bf0885181c
juscodit <juscodit@gmail.com>
2016-04-23 10:53:29 +0800

    [boost_filesystem]: 加入获取 filename demo

--- cpp/boost_filesystem.cpp
+++ cpp/boost_filesystem.cpp
@@ -15,6 +15,12 @@ int main(int argc, char *argv[])
 
     // get file size
     std::cout << boost::filesystem::file_size("boost_filesystem.cpp") << std::endl;
+
+    // get filename
+    boost::filesystem::path p("/home/crs/kgengine.tar.gz");
+    std::cout << "path" << p << std::endl;
+    std::cout << "filename " << p.filename().string() << std::endl;
+    std::cout << "filename without extension " << p.stem().string() << std::endl;
     
     return 0;
 }
