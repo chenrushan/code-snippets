302c59d940ad67d60bc57f307b7b2a45f96f2641
juscodit <juscodit@gmail.com>
2016-04-28 10:03:27 +0800

    [deal_with_command_line_arg.py]: update

--- python/deal_with_command_line_arg.py
+++ python/deal_with_command_line_arg.py
@@ -5,9 +5,14 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Help iSCSI deployment.')
+    # choices 就是这个选项只能取 choices 数组中给出的值
     parser.add_argument('-s', '--service', choices=['target', 'initiator'],
                         default=None, help='type of service deployment')
     parser.add_argument('--test', action='store_true', default=False,
                         help='Test the code against a local mkcloud installation')
     args = parser.parse_args()
+
+    print args.service
+    if args.test:
+        print "test is true"
 
