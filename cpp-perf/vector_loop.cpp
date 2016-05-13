dc9576221f20884c8dd29773ca4f8e76bdcb5377
juscodit <juscodit@gmail.com>
2016-05-13 19:39:08 +0800

    [vector_loop]: update

--- cpp-perf/vector_loop.cpp
+++ cpp-perf/vector_loop.cpp
@@ -5,9 +5,10 @@ using namespace std;
 // ----------------------------------------------------------------------
 // 比较一下不同方式遍历 vector 的性能, typical resutl:
 // 
-// (1). 51450.3µs
-// (2). 93288.5µs
-// (3). 51779.4µs
+// (1). 50519.9µs
+// (2). 79243.7µs
+// (3). 50520.5µs
+// (4). 50406.2µs
 //
 // 没想到 (2) 这么慢
 // ----------------------------------------------------------------------
@@ -22,6 +23,7 @@ int main(int argc, char *argv[])
         Timing t;
         for_each(rows.begin(), rows.end(), [&i](int &e){e=i++;});
     }
+    cout << i << endl;
 
     // (2)
     {
@@ -31,6 +33,7 @@ int main(int argc, char *argv[])
             rows[j] = i++;
         }
     }
+    cout << i << endl;
 
     // (3)
     {
@@ -40,6 +43,17 @@ int main(int argc, char *argv[])
             r = i++;
         }
     }
+    cout << i << endl;
+
+    // (4)
+    {
+        Timing t;
+        i = 0;
+        for (auto it = rows.begin(); it != rows.end(); ++it) {
+            *it = i++;
+        }
+    }
+    cout << i << endl;
 
     return 0;
 }
