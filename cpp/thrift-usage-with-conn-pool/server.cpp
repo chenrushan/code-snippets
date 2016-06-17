aa6a2bfa48a20b2e7640109259b148b4f872de5d
juscodit <juscodit@gmail.com>
2016-06-17 14:16:58 +0800

    [server]: modify to use class in tserver.h

--- cpp/thrift-usage-with-conn-pool/server.cpp
+++ cpp/thrift-usage-with-conn-pool/server.cpp
@@ -1,23 +1,10 @@
-#include <thrift/concurrency/ThreadManager.h>
-#include <thrift/concurrency/PosixThreadFactory.h>
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/server/TThreadPoolServer.h>
-#include <thrift/server/TThreadedServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TTransportUtils.h>
-
 #include "gen-cpp/HelloSevice.h"
+#include "tserver.h"
 
-using namespace apache::thrift;
-using namespace apache::thrift::concurrency;
-using namespace apache::thrift::protocol;
-using namespace apache::thrift::transport;
-using namespace apache::thrift::server;
+using namespace util;
 
 class HelloEngine : public HelloSeviceIf {
 public:
-
     void say_hello(Response &resp, const Request &req) {
         char buf[256];
         for (auto i = 0; i < req.msg_size-1 && i < 256; ++i) {
@@ -33,7 +20,7 @@ public:
 
 class HelloFactory : public HelloSeviceIfFactory {
 public:
-    HelloSeviceIf *getHandler(const TConnectionInfo &conn) {
+    HelloSeviceIf *getHandler(const apache::thrift::TConnectionInfo &conn) {
         return (HelloSeviceIf *)(new HelloEngine());
     }
 
@@ -44,23 +31,13 @@ public:
 
 int main(int argc, char *argv[])
 {
-    boost::shared_ptr<ThreadManager> threadManager =
-        ThreadManager::newSimpleThreadManager(50);
-    boost::shared_ptr<PosixThreadFactory> threadFactory =
-        boost::shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
-    threadManager->threadFactory(threadFactory);
-    threadManager->start();
+    MultiThreadedThriftServerConfig conf;
+    conf.num_threads = 10;
+    conf.port = 19999;
 
-    boost::shared_ptr<HelloFactory> handler(new HelloFactory());
-    boost::shared_ptr<TProcessorFactory> processor(new HelloSeviceProcessorFactory(handler));
-    boost::shared_ptr<TServerTransport> serverTransport(new TServerSocket(19999));
-    boost::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-    boost::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-
-    TThreadPoolServer server(processor, serverTransport,
-            transportFactory, protocolFactory, threadManager);
-    fprintf(stderr, "start engine server ...\n");
+    MultiThreadedThriftServer<HelloFactory, HelloSeviceProcessorFactory> server(conf);
     server.serve();
+
     return 0;
 }
 
