785ca97767e022cc01adba6af97c2db4550aea81
juscodit <juscodit@gmail.com>
2016-08-13 14:49:23 +0800

    [server.cpp]: update

--- cpp/websocketpp-demo/server.cpp
+++ cpp/websocketpp-demo/server.cpp
@@ -3,6 +3,14 @@
 
 #include <iostream>
 
+// ======================================================================
+// 这个程序实现了一个这样的 websocket server 框架
+// 1. server 端接受请求 --> 出发 on_message 调用 --> on_message 将 message
+//    加入一个 message pool
+// 2. 另外有一个独立的 thread，不断从 message pool 中取 message 并不断分给
+//    worker thread 打印
+// ======================================================================
+
 class WebsocketServer {
     using connection_hdl = websocketpp::connection_hdl;
     using websocket_server = websocketpp::server<websocketpp::config::asio>;
@@ -69,36 +77,23 @@ private:
 
 class ThreadPool {
 public:
-    ThreadPool(size_t nthreads);
-    ~ThreadPool();
 
-    template<class F> void execute(F f);
+    ThreadPool(size_t nthreads) : work(service) {
+        for (size_t i = 0; i < nthreads; ++i) {
+            threadpool.create_thread(
+                    boost::bind(&boost::asio::io_service::run, &service));
+        }
+    }
+
+    ~ThreadPool() { service.stop(); threadpool.join_all(); }
+
+    template<class F> void execute(F f) { service.post(f); }
 
 private:
     boost::asio::io_service service;
     boost::thread_group threadpool;
     boost::asio::io_service::work work;
 };
-
-ThreadPool::ThreadPool(size_t nthreads)
-    : work(service)
-{
-    for (size_t i = 0; i < nthreads; ++i) {
-        threadpool.create_thread(boost::bind(&boost::asio::io_service::run, &service));
-    }
-}
-
-ThreadPool::~ThreadPool()
-{
-    service.stop();
-    threadpool.join_all();
-}
-
-template<class F>
-void ThreadPool::execute(F f)
-{
-    service.post(f);
-}
 
 // ======================================================================
 
@@ -120,6 +115,8 @@ struct Message {
     }
 };
 
+// ----------------------------------------------------------------------
+
 void on_message(websocketpp::connection_hdl hdl, const std::string &msg,
                 OneWriterMultiReaderObjectPool<Message> &msg_pool)
 {
@@ -127,19 +124,40 @@ void on_message(websocketpp::connection_hdl hdl, const std::string &msg,
     msg_pool.add({hdl, msg});
 }
 
-void print_message(OneWriterMultiReaderObjectPool<Message> &msg_pool)
-{
-    std::cerr << "print_message" << std::endl;
-    while (true) {
-        msg_pool.for_each([](const Message &msg) {
-            std::cout << "***" << msg.msg << " from "
-                      << msg.hdl.lock().get() << std::endl;
-            return 0;
-        });
-        std::cout << "==========" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+// ----------------------------------------------------------------------
+
+class MessagePrinter {
+public:
+    MessagePrinter(OneWriterMultiReaderObjectPool<Message> &msg_pool)
+        : msg_pool_(msg_pool), thpool_(10) {}
+
+    void operator()() {
+        while (true) {
+            msg_pool_.for_each([&](const Message &msg) {
+                thpool_.execute(std::bind(
+                        &MessagePrinter::print_message, this, std::cref(msg)));
+                return 0;
+            });
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
     }
-}
+
+private:
+
+    void print_message(const Message &msg) {
+        std::lock_guard<std::mutex> lock(console_mutex);
+        std::cout << "[" << std::this_thread::get_id() << "]" << msg.msg
+                  << " from " << msg.hdl.lock().get() << std::endl;
+    }
+
+private:
+
+    OneWriterMultiReaderObjectPool<Message> &msg_pool_;
+    ThreadPool thpool_;
+    std::mutex console_mutex;
+};
+
+// ----------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
@@ -147,8 +165,13 @@ int main(int argc, char *argv[])
     using websocketpp::lib::placeholders::_2;
 
     OneWriterMultiReaderObjectPool<Message> msg_pool;
+    MessagePrinter msg_printer(msg_pool);
 
-    std::thread(std::bind(print_message, std::ref(msg_pool))).detach();
+    // 需要用 std::ref 否则会调用 MessgePrinter 的 copy constructor，而
+    // MessgePrinter 的 copy constructor 是 deleted 的，因为其中包含的
+    // msg_pool_ 里头有个 mutex，这就导致外面的 MessagePrinter 也是不能
+    // copy 的
+    std::thread(std::ref(msg_printer)).detach();
 
     try {
         WebsocketServer server(16868, std::bind(on_message, _1, _2, std::ref(msg_pool)));
