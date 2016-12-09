20018b5c463492c8c5f6dcb9968c8ea4f590cbfb
chenrushan <chenrushan@dingfudata.com>
2016-12-09 11:23:06 +0800

    [deal_with_command_line_arg.py]: add required usage

--- python/deal_with_command_line_arg.py
+++ python/deal_with_command_line_arg.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Help iSCSI deployment.')
     # choices 就是这个选项只能取 choices 数组中给出的值
     parser.add_argument('-s', '--service', choices=['target', 'initiator'],
-                        default=None, help='type of service deployment')
+                        default=None, required=True, help='type of service deployment')
     parser.add_argument('--test', action='store_true', default=False,
                         help='Test the code against a local mkcloud installation')
     args = parser.parse_args()
