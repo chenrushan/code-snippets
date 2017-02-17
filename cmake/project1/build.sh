13994de97ee618a7cfac6cf60dd06d653f3fce2a
chenrushan <chenrushan@dingfudata.com>
2017-02-17 11:05:03 +0800

    [build.sh]: add build script

---
+++ cmake/project1/build.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# ======================================================================
+# 参数设置 build_type
+# ======================================================================
+
+build_type=Debug
+if [[ $# -eq 1 ]]; then
+    build_type=$1
+fi
+
+if [[ "$build_type" == "r" ]] || [[ "$build_type" == "release" ]]; then
+    build_type=Release
+fi
+
+if [[ "$build_type" == "d" ]] || [[ "$build_type" == "debug" ]]; then
+    build_type=Debug
+fi
+
+echo "BUILD_TYPE: ${build_type}"
+
+# ======================================================================
+# 创建必要的目录
+# ======================================================================
+
+[[ -d .build/${build_type} ]] || mkdir -p .build/${build_type}
+if [[ $? -ne 0 ]]; then
+    echo "fail to mkdir build"
+    exit 1
+fi
+[[ -d bin ]] || mkdir bin
+if [[ $? -ne 0 ]]; then
+    echo "fail to mkdir bin"
+    exit 1
+fi
+
+# ======================================================================
+# 准备开始编译
+# ======================================================================
+
+dir=$(pwd)
+echo "project dir: ${dir}"
+cd .build/${build_type}
+cmake -D CMAKE_INSTALL_PREFIX=${dir} -D CMAKE_BUILD_TYPE=${build_type} ${dir}
+make install
+
